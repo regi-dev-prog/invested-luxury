36e6df54389f2ab1dcd3a40fce5f3b69d3b93cad
Regev's Place <DELL1@Regevs-MacBook-Pro.local>
2025-12-05 15:29:54 +0100

    Fix Sanity image URL builder import

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from 'next-sanity';
-import { createImageUrlBuilder } from '@sanity/image-url';
+import imageUrlBuilder from '@sanity/image-url';
 
 const projectId = '4b3ap7pf';
 const dataset = 'production';
@@ -13,11 +13,11 @@ export const client = createClient({
 });
 
 // Image URL builder
-const imageBuilder = createImageUrlBuilder({ projectId, dataset });
+const builder = imageUrlBuilder(client);
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function urlFor(source: any) {
-  return imageBuilder.image(source);
+  return builder.image(source);
 }
 
 // Preview client (for drafts)
