0b4fec96604a23efa7c1aaa00ba9e75f584ad91c
Regev's Place <DELL1@Regevs-MacBook-Pro.local>
2026-01-24 15:22:00 +0100

    Remove GTM implementation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -107,13 +107,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-K8TKJVVG');</script>
-<!-- End Google Tag Manager -->
+       
         <meta name="p:domain_verify" content="e9193475d2f8aacdc5222b6ca882ca0e"/>
         {/* Google Analytics */}
         <Script
@@ -142,10 +136,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         />
       </head>
       <body className="min-h-screen flex flex-col">
-        <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K8TKJVVG"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+        
         <ComingSoon />
         <Header />
         <main className="flex-1">{children}</main>
