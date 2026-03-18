ea6b9d2fa2a5152c3f7293cda02143648b9f727e
regi-dev-prog <digital@regevsela.com>
2026-03-18 10:48:33 +0100

    fix(seo): add OG image to category pages

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -57,11 +57,13 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
       url: canonicalUrl,
       siteName: 'InvestedLuxury',
       type: 'website',
+      images: [{ url: 'https://investedluxury.com/og-image.jpg', width: 1200, height: 630, alt: 'InvestedLuxury' }],
     },
     twitter: {
       card: 'summary_large_image',
       title: category.metaTitle,
       description: category.metaDescription,
+      images: ['https://investedluxury.com/og-image.jpg'],
     },
   }
 }
