0285c847ca96ac4c2ccd26871f22e2e9fc807ece
regi-dev-prog <digital@regevsela.com>
2026-07-18 16:39:11 +0200

    Cleanup: remove dead file src/sanity/schemaTypes/index.ts (nothing imports it; duplicate of live code)

--- src/sanity/schemaTypes/index.ts
+++
@@ -1,8 +0,0 @@
-import brand from './brand'
-import category from './category'
-import product from './product'
-import author from './author'
-import article from './article'
-import featuredProduct from './featuredProduct'
-
-export const schemaTypes = [brand, category, product, author, article, featuredProduct]
